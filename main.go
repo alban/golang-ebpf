c42e7ea7c05e062d49dd30d89e56c2d28fc63b79
Alban Crequy <alban@kinvolk.io>
2016-11-03 20:26:18 +0100

    use Golang callbacks

--- main.go
+++ main.go
@@ -16,42 +16,16 @@ import (
 #include <bcc/libbpf.h>
 #include <bcc/perf_reader.h>
 #include <stdio.h>
+
 void *bpf_open_perf_buffer(perf_reader_raw_cb raw_cb, void *cb_cookie, int pid, int cpu);
 
+extern void tcpEventCb();
+
 struct tcp_event_t {
-        unsigned long long pid;
-        unsigned long long dummy;
+        uint64_t pid;
+        uint64_t dummy;
 };
 
-void callback(int cpu, void *data, int size) {
-	struct tcp_event_t *ev = (struct tcp_event_t *)data;
-	uint32_t pid, tid;
-
-	if (size != sizeof(*ev)) {
-		printf("Invalid tcp_event_t event\n");
-		return;
-	}
-
-	pid = ev->pid & 0xffffffff;
-	tid = (ev->pid >>32) & 0xffffffff;
-
-	printf("Hello bpf, my pid is %d and tid is %d\n", pid, tid);
-}
-
-void raw_cb(void *cb_cookie, void *raw, int raw_size) {
-	// See src/cc/perf_reader.c:parse_sw()
-	// struct {
-	//     uint32_t size;
-	//     char data[0];
-	// };
-
-	if (raw_size != sizeof(uint32_t) + sizeof(struct tcp_event_t)) {
-		printf("Invalid perf event: raw_size=%d != %lu + %lu\n", raw_size, sizeof(uint32_t), sizeof(struct tcp_event_t));
-		return;
-	}
-
-	callback(0, raw, raw_size - sizeof(uint32_t));
-}
 */
 import "C"
 
@@ -90,10 +64,41 @@ int kprobe__tcp_v4_connect(struct pt_regs *ctx, struct sock *sk)
 };
 `
 
+func tcpEventCallback(cpu int, tcpEvent *C.struct_tcp_event_t) {
+	pid := tcpEvent.pid & 0xffffffff
+	tid := (tcpEvent.pid >> 32) & 0xffffffff
+
+	fmt.Printf("Hello bpf, my pid is %d and tid is %d\n", pid, tid)
+}
+
+//export tcpEventCb
+func tcpEventCb(cb_cookie unsafe.Pointer, raw unsafe.Pointer, raw_size C.int) {
+	// See src/cc/perf_reader.c:parse_sw()
+	// struct {
+	//     uint32_t size;
+	//     char data[0];
+	// };
+
+	var tcpEvent C.struct_tcp_event_t
+
+	if int(raw_size) != 4+int(unsafe.Sizeof(tcpEvent)) {
+		fmt.Printf("Invalid perf event: raw_size=%d != %lu + %lu\n", raw_size, 4, unsafe.Sizeof(tcpEvent))
+		return
+	}
+
+	tcpEventCallback(0, (*C.struct_tcp_event_t)(raw))
+}
+
 func initPerfMap(table *bpf.BpfTable) []*C.struct_perf_reader {
 	fd := table.Config()["fd"].(int)
 	key_size := table.Config()["key_size"].(uint64)
 	leaf_size := table.Config()["leaf_size"].(uint64)
+
+	if key_size != 4 || leaf_size != 4 {
+		fmt.Printf("Wrong size\n")
+		os.Exit(1)
+	}
+
 	key := make([]byte, key_size)
 	leaf := make([]byte, leaf_size)
 	keyP := unsafe.Pointer(&key[0])
@@ -104,14 +109,7 @@ func initPerfMap(table *bpf.BpfTable) []*C.struct_perf_reader {
 	cpu := 0
 	res := 0
 	for res == 0 {
-		// Get a new perf fd
-		// https://github.com/torvalds/linux/blob/v4.8/include/uapi/linux/perf_event.h
-		// Type: PERF_TYPE_SOFTWARE = 1
-		// config: PERF_COUNT_SW_BPF_OUTPUT = 10
-		const PERF_TYPE_SOFTWARE uint32 = 1
-		const PERF_COUNT_SW_BPF_OUTPUT uint64 = 10
-
-		reader := C.bpf_open_perf_buffer((*[0]byte)(C.raw_cb), nil, -1, C.int(cpu))
+		reader := C.bpf_open_perf_buffer((*[0]byte)(C.tcpEventCb), nil, -1, C.int(cpu))
 		if reader == nil {
 			fmt.Printf("failed to get reader\n")
 			os.Exit(1)
@@ -120,8 +118,6 @@ func initPerfMap(table *bpf.BpfTable) []*C.struct_perf_reader {
 		perfFd := C.perf_reader_fd(reader)
 
 		readers = append(readers, (*C.struct_perf_reader)(reader))
-
-		fmt.Printf("perfFd=%v\n", perfFd)
 
 		leaf[0] = byte(perfFd) // TODO: how does Go work again?
 		leaf[1] = 0
@@ -158,13 +154,12 @@ func main() {
 	}
 
 	t := bpf.NewBpfTable(0, m)
-	fmt.Printf("table: %q %q\n%v\n", t.ID(), t.Name(), t.Config())
 	readers := initPerfMap(t)
 
 	fmt.Printf("Ready.\n")
 
 	for {
-		C.perf_reader_poll(4, &readers[0], -1)
+		C.perf_reader_poll(C.int(len(readers)), &readers[0], -1)
 	}
 
 	time.Sleep(time.Second * 1000)
